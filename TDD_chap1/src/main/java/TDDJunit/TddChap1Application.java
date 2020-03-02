262c0e3ed3da05ce360acf40794b936a5ce2afbf
Long Nguyen <nguyenhlongvn@gmail.com>
2020-03-01 19:23:16 -0600

    Initial

---
+++ TDD_chap1/src/main/java/TDDJunit/TddChap1Application.java
@@ -0,0 +1,13 @@
+package TDDJunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddChap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddChap1Application.class, args);
+	}
+
+}
---
+++ TDD_chap1/src/test/java/TDDJunit/TddChap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package TDDJunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddChap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
